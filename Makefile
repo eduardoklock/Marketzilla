d99622f55dd2468c2cb756810edbbe78f319adde
Daka Teavi <daka@localhost.localdomain>
2014-05-25 20:09:25 -0300

    boa sorte bulba

--- Makefile
+++ Makefile
@@ -7,6 +7,9 @@ INCLUDE=-Iinclude
 # program souce files
 SOURCES=source/marketzilla/*.cpp
 
+# build source
+BUILD_SOUCES=$(SOURCES) main.cpp
+
 # test source files
 TEST_SOURCES=$(SOURCES) test/marketzilla/*.cpp
 
@@ -18,10 +21,17 @@ FLAGS=-std=c++11
 
 # executable name
 EXECUTABLE=marketzilla
+TEST_EXEC=marketzillaTest
 
-all: $(TEST_SOURCES)
-	$(COMPILER) -o $(EXECUTABLE) $(TEST_SOURCES) $(INCLUDE) $(LIBS) $(FLAGS)
+all: test build
+
+test: $(TEST_SOURCES)
+	$(COMPILER) -o $(TEST_EXEC) $(TEST_SOURCES) $(INCLUDE) $(LIBS) $(FLAGS)
+	./$(TEST_EXEC)
+
+build: $(BUILD_SOUCES)
+	$(COMPILER) -o $(EXECUTABLE) $(BUILD_SOUCES) $(INCLUDE) $(LIBS) $(FLAGS)
 	./$(EXECUTABLE)
 
 clean:
-	rm -rf *.o $(EXECUTABLE)
+	rm -rf *.o $(EXECUTABLE) $(TEST_EXEC)
---
+++ main.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+    string marketName;
+    int timeOfSimulation;
+    int timeOfClientsArive;
+    int numberOfCashiers;
+    fstream file;
+    file.open("data");
+
+    if(file.is_open())
+    {
+        getline(file, marketName);
+        file >> timeOfSimulation;
+        file >> timeOfClientsArive;
+        file >> numberOfCashiers;
+
+        cout << "Markte Name: "<< marketName << endl;
+        cout << "Simulation Time: " << timeOfSimulation << endl;
+        cout << "Clients arraival Delay: " <<timeOfClientsArive <<endl;
+        cout << "Number of Cashiers: "<< numberOfCashiers << endl;
+
+        int salary;
+        int eficiency;
+        string name;
+        for(int i = 0; i < numberOfCashiers; ++i)
+        {
+            file >> name;
+            file >> eficiency;
+            file >> salary;
+            //market.add(Cashier(name,salary,eficiency));
+            cout << "Cashier: " << name << " " << eficiency << " " << salary << endl;
+        }
+
+        file.close();
+    }
+    else
+    {
+        cout << "file not finded" << endl;
+    }
+    
+    return 0;
+}
