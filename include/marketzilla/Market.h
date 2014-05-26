b2765b769a823a6833c64517b7f4bdf5ed3c939a
Eduardo Klock <eduardo.klock@gmail.com>
2014-05-26 00:06:24 -0300

    cause i got high

--- include/marketzilla/Market.h
+++ include/marketzilla/Market.h
@@ -1,12 +1,25 @@
 #ifndef _Market_h_
 #define _Market_h_
 
+#include "Cashier.h"
+
 namespace marketzilla {
 
 class Market{
 public:
+    Market(string, int, int, CircularList<Cashier>*);
+    void creatCashiers(const string&, double, int);
+    void simulation();
+    bool allQueueEmpty();
 	
 private:
+    CircularList<Cashier>* cashiers;
+    int currentTime;
+    int givenUpClients;
+    int totalInjury;
+    int totalSimulationTime;
+    int arrivalTimeCliet;
+    string marketName;
 
 };
 
---
+++ source/marketzilla/Maket.cpp
@@ -0,0 +1,60 @@
+#include "marketzilla/Market.h"
+#include "marketzilla/ClientFactory.h"
+#include <string>
+
+
+namespace marketzilla {
+
+Market::Market(string marketName,int totalSimulationTime, int arrivalTimeCliet, CircularList<Cashier>* cashiers):
+marketName(marketName),
+totalSimulationTime(totalSimulationTime),
+arrivalTimeCliet(arrivalTimeCliet),
+cashiers(cashiers),
+currentTime(0),
+givenUpClients(0),
+totalInjury(0)
+{}
+
+void Market::creatCashiers(const string& name, double salary, int eficiency)
+{
+    Attendant attendant = Attendant(name, salary, eficiency);
+    Cashier cashier = Cashier(attendant);
+    cashiers->push_back(cashier);
+}
+
+bool Market::allQueueEmpty()
+{
+    return true;
+}
+
+void Market::simulation()
+{
+    ClientFactory factory = ClientFactory();
+    while(currentTime != totalSimulationTime && allQueueEmpty())
+    {
+        if(currentTime % arrivalTimeCliet == 0 && currentTime < totalSimulationTime)
+        {
+            Client client = factory.makeClient();
+            if(!client.chooseCashier(*cashiers))
+            {
+                ++givenUpClients;
+                totalInjury = totalInjury + (client.totalItemsValue()*3);
+            }
+            else
+            {
+                //calculo do cashier para setar tempo de saida....
+            }
+        }
+        auto i = cashiers->begin();
+        while(i != cashiers->end())
+        {
+            i->update(currentTime);
+            ++i;
+        }
+        ++currentTime;
+
+    }
+}
+
+
+}
