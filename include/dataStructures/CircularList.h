21a24f265fd69f0455b172233bf7b37e80ea95fa
Eduardo Klock <eduardo.klock@gmail.com>
2014-05-26 17:12:47 -0300

    circulo do s2

--- include/dataStructures/CircularList.h
+++ include/dataStructures/CircularList.h
@@ -1,8 +1,8 @@
 #ifndef _CircularList_h_
 #define _CircularList_h_
 
+#include <iostream>
 #include <stdexcept>
-#include "List.h"
 
 namespace dataStructures {
 
@@ -18,58 +18,52 @@ class CircularList {
         T item;
     };
 
-    node *sentinel;
+    node* _sentinel;
     int count;
 
     template<typename U>
     class iterator_base {
     public:
-        iterator_base(node* ptr) :
+        iterator_base(node* ptr = 0) :
                 ptr(ptr) {
         }
 
-        iterator_base& operator=(const iterator_base& other) {
-            if (!(*this == other))
-                this->ptr = other.ptr;
-            return *this;
-        }
-
         iterator_base& operator++() {
-            this->ptr = this->ptr->succ;
+            ptr = ptr->succ;
             return *this;
         }
 
-        iterator_base& operator++(int) {
-            iterator_base other(this);
-            ++this;
+        iterator_base operator++(int) {
+            iterator_base other(*this);
+            ptr = ptr->succ;
             return other;
         }
 
         iterator_base& operator--() {
-            this->ptr = this->ptr->pred;
+            ptr = ptr->pred;
             return *this;
         }
 
-        iterator_base& operator--(int) {
-            iterator_base other(this);
-            --this;
+        iterator_base operator--(int) {
+            iterator_base other(*this);
+            ptr = ptr->pred;
             return other;
         }
 
         bool operator==(const iterator_base& other) const {
-            return this->ptr == other.ptr;
+            return ptr == other.ptr;
         }
 
         bool operator!=(const iterator_base& other) const {
-            return !(*this == other);
+            return ptr != other.ptr;
         }
 
         U& operator*() const {
-            return this->ptr->item;
+            return ptr->item;
         }
 
         U* operator->() const {
-            return &(this->ptr->item);
+            return &(ptr->item);
         }
 
     private:
@@ -77,37 +71,49 @@ class CircularList {
     };
 
 public:
-    CircularList() :
-            sentinel(new node(0, 0, T())), count(0) {
-        sentinel->succ = sentinel->pred = sentinel;
+    CircularList () :
+            _sentinel(new node(0, 0, T())), count(0) {
+        _sentinel->succ = _sentinel->pred = _sentinel;
     }
 
     virtual ~CircularList() {
-        while (sentinel->succ != sentinel) {
-            this->sentinel->succ = this->sentinel->succ->succ;
-            delete this->sentinel->succ->pred;
+        while (_sentinel->succ != _sentinel) {
+            this->_sentinel->succ = this->_sentinel->succ->succ;
+            delete this->_sentinel->succ->pred;
         }
     }
 
     bool empty() const {
-        return this->count == 0;
+        return count == 0;
     }
 
     int size() const {
-        return this->count;
+        return count;
+    }
+
+    T at(int position) const {
+        if (position == 0) {
+            return this->front();
+        } else if (position == count - 1) {
+            return this->back();
+        } else if (position < 0 || position >= count) {
+            throw std::out_of_range("Invalid position.");
+        }
+
+        node* p = _sentinel->succ;
+        for (int i = 0; i < position; ++i)
+            p = p->succ;
+        return p->item;
     }
 
     T back() const {
-        return sentinel->pred->item;
+        return _sentinel->pred->item;
     }
 
     T front() const {
-        return sentinel->succ->item;
+        return _sentinel->succ->item;
     }
 
-    /**
-     * Operações de inserção
-     */
     void push(int position, const T& item) {
         if (position == 0) {
             push_front(item);
@@ -119,57 +125,38 @@ public:
             throw std::out_of_range("Inserting out of bounds");
         }
 
-        node* aux = sentinel->succ;
+        node* aux = _sentinel->succ;
         for (int i = 0; i < position; ++i) {
             aux = aux->succ;
         }
 
-        /*
-         * Mudar o predecessor do sucessor de aux ->
-         *** Sucessor do predecessor de aux aponta para novo item
-         *** Predecessor de aux aponta para novo item
-         * Tempo: O(1)
-         */
         aux->pred = aux->pred->succ = new node(aux->pred, aux, item);
         ++count;
     }
 
     void push_back(const T& item) {
-        /*
-         * Mudar o predecessor do sucessor da sentinela ->
-         *** Sucessor do predecessor da sentinela aponta para novo item
-         *** Predecessor da sentinela aponta para novo item
-         */
-        sentinel->pred = sentinel->pred->succ = new node(sentinel->pred,
-                sentinel, item);
+        _sentinel->pred = _sentinel->pred->succ = new node(_sentinel->pred,
+                _sentinel, item);
         ++this->count;
     }
 
     void push_front(const T& item) {
-        /*
-         * Mudar o sucessor do predecessor da sentinela ->
-         *** Predecessor do sucessor da sentinela aponta para novo item
-         *** Sucessor da sentinela aponta para novo item
-         */
-        sentinel->succ = sentinel->succ->pred = new node(sentinel,
-                sentinel->succ, item);
+        _sentinel->succ = _sentinel->succ->pred = new node(_sentinel,
+                _sentinel->succ, item);
         ++this->count;
     }
 
-    /**
-     * Operações de remoção
-     */
     T pop_back() {
-        if (this->count) {
-            T removed = this->sentinel->pred->item;
-            this->sentinel->pred = this->sentinel->pred->pred;
-            delete this->sentinel->pred->succ;
-            this->sentinel->pred->succ = this->sentinel;
-            --this->count;
-            return removed;
-        } else {
-            throw std::out_of_range("Empty CircularList.");
+        if (this->empty()) {
+            throw std::out_of_range("Empty list.");
         }
+
+        T removed = this->_sentinel->pred->item;
+        this->_sentinel->pred = this->_sentinel->pred->pred;
+        delete this->_sentinel->pred->succ;
+        this->_sentinel->pred->succ = this->_sentinel;
+        --this->count;
+        return removed;
     }
 
     T pop(int position) {
@@ -177,10 +164,11 @@ public:
             return this->pop_front();
         } else if (position == this->count - 1) {
             return this->pop_back();
-        } else if (position < 0 && position >= this->count) {
+        } else if (position < 0 || position >= this->count) {
             throw std::out_of_range("Invalid position.");
         }
-        node* exNode = this->sentinel->succ; // será o nó que estava na posição escolhida
+
+        node* exNode = this->_sentinel->succ; // será o nó que estava na posição escolhida
         for (int i = 0; i < position; ++i) {
             exNode = exNode->succ;
         }
@@ -194,53 +182,52 @@ public:
 
     T pop_front() {
         if (this->empty()) {
-            throw std::out_of_range("Empty CircularList.");
-        } else {
-            T removed = this->sentinel->item;
-            this->sentinel->succ = this->sentinel->succ->succ;
-            delete this->sentinel->succ->pred;
-            this->sentinel->succ->pred = this->sentinel;
-            --this->count;
-            return removed;
+            throw std::out_of_range("Empty list.");
         }
+
+        T removed = this->_sentinel->succ->item;
+        this->_sentinel->succ = this->_sentinel->succ->succ;
+        delete this->_sentinel->succ->pred;
+        this->_sentinel->succ->pred = this->_sentinel;
+        --this->count;
+        return removed;
     }
 
     typedef iterator_base<T> iterator;
     typedef iterator_base<const T> const_iterator;
 
     iterator begin() {
-        return iterator(this->sentinel->succ);
+        return iterator(_sentinel->succ);
     }
 
     iterator end() {
-        return iterator(this->sentinel);
+        return iterator(_sentinel);
     }
 
     iterator rbegin() {
-        return iterator(this->sentinel->pred);
+        return iterator(_sentinel->pred);
     }
 
     iterator rend() {
-        return iterator(this->sentinel);
+        return iterator(_sentinel);
     }
 
     const_iterator begin() const {
-        return const_iterator(this->sentinel->succ);
+        return const_iterator(_sentinel->succ);
     }
 
     const_iterator end() const {
-        return const_iterator(this->sentinel);
+        return const_iterator(_sentinel);
     }
 
     const_iterator rbegin() const {
-        return const_iterator(this->sentinel->pred);
+        return const_iterator(_sentinel->pred);
     }
 
     const_iterator rend() const {
-        return const_iterator(this->sentinel);
+        return const_iterator(_sentinel);
     }
 };
-
 }
 
-#endif
+#endif 
